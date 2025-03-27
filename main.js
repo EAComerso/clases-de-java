83c7c9a17f4ddff68c03ea1fd5b6fca968e77597
esteban.comerso <esteban.conerso@gmail.com>
2025-03-27 20:04:24 -0300

    ingreso de personajes

---
+++ main.js
@@ -0,0 +1,13 @@
+//let personaje1 = "Goku"
+//let personaje2 = "Vegeta"
+//let personaje3 = "Picollo"
+//let personaje4 = "Krillin"
+//let personaje5 = "Freezer"
+//let personaje6 = "Gohan"
+//let personaje7 = "Trunks Futuro"
+//let personaje8 = "Cell"
+//let personaje9 = "Majin bu"
+//let personaje10 = "Ten shin han"
+
+const Humanos = ['Krillin','Ten shin han']
+consolelog 
